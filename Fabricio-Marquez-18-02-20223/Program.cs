9cecba31d15718f3168910f5123a641e1c6d34a7
FabricioMarquez123 <114788319+FabricioMarquez123@users.noreply.github.com>
2023-02-18 11:30:24 -0600

    Ejericio

---
+++ Fabricio-Marquez-18-02-20223/Program.cs
@@ -0,0 +1,10 @@
+﻿using entidades;
+using negocio;
+
+negocio logica=new num_letras();
+entidades entidades=new letras();
+
+
+console.writeline("Ingrese letras");
+console.readline(letras);
+console.writeline(num_letras(letras));
---
+++ Fabricio-Marquez-18-02-20223/negocio/logica.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using entidades;
+
+namespace negocio
+{
+    public class num_letras
+    {
+        public int numero_letras(letras letra){
+            return console.writeline(length.letra);
+        }
+    }
+}
